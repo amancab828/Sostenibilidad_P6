b61be343edd8262e557511bb5f6220b40c9b8ab3
Dariac04 <danielvarce04@gmail.com>
2026-03-07 08:40:12 +0100

    Implementación de empresas en pagina empresas

--- empresas.php
+++ empresas.php
@@ -23,6 +23,39 @@
         <h2>Información sobre empresas colaboradoras que apoyan la sostenibilidad</h2>
 
         <p></p>
+        <div class="empresas">
+
+            <div class="empresas-card dell">
+                <img src="https://1000logos.net/wp-content/uploads/2017/07/Dell-Logo.png">
+                <h3>Dell</h3>
+                <button>Ver</button>
+            </div>
+
+            <div class="empresas-card hp">
+                <img src="https://1000logos.net/wp-content/uploads/2017/02/HP-Log%D0%BE.png">
+                <h3>HP</h3>
+                <button>Ver</button>
+            </div>
+
+            <div class="empresas-card lenovo">
+                <img src="https://1000logos.net/wp-content/uploads/2017/03/Lenovo-Logo.png">
+                <h3>Lenovo</h3>
+                <button>Ver</button>
+            </div>
+
+            <div class="empresas-card acer">
+                <img src="https://1000logos.net/wp-content/uploads/2016/09/Acer-Logo.png">
+                <h3>Acer</h3>
+                <button>Ver</button>
+            </div>
+
+            <div class="empresas-card msi">
+                <img src="https://1000logos.net/wp-content/uploads/2018/10/MSI-Logo.png">
+                <h3>MSI</h3>
+                <button>Ver</button>
+            </div>
+
+        </div>
     </div>
     
     <!-- Footer -->
